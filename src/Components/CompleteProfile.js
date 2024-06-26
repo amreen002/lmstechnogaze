d7865713c9f999b917aa7801e0672a6084d8c895
Aakash775 <akash@technogaze.com>
2024-06-26 17:12:45 +0530

    Complete profile page with css

--- src/Components/CompleteProfile.js
+++ src/Components/CompleteProfile.js
@@ -26,47 +26,66 @@ const CompleteProfile = () => {
   return (
     <div>
         <Navbarmenu />
-      <h2>Complete Your Profile</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
+        <section className='py-5'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12 col-xl-12 col-lg-12 col-md-12'>
+            
+      <form className="form-control llbl" onSubmit={handleSubmit}>
+       
+          <div className='cus_head'>
+          <h2>Complete Your Profile</h2>
+          </div>
+          <div className='pb-3'>
           <label>First Name</label>
           <input
             type="text"
             name="firstName"
             value={profile.firstName}
             onChange={handleChange}
+            className='form-control'
           />
         </div>
-        <div>
+        <div  className='pb-3'>
           <label>Last Name</label>
           <input
             type="text"
             name="lastName"
             value={profile.lastName}
             onChange={handleChange}
+            className='form-control'
           />
         </div>
-        <div>
+        <div className='pb-3'>
           <label>Phone Number</label>
           <input
             type="text"
             name="phoneNumber"
             value={profile.phoneNumber}
             onChange={handleChange}
+            className='form-control'
           />
         </div>
-        <div>
+        <div className='pb-3'>
           <label>Address</label>
           <input
             type="text"
             name="address"
             value={profile.address}
             onChange={handleChange}
+            className='form-control'
           />
         </div>
-        <button type="submit">Complete Profile</button>
+        <button type="submit" className=' btn btn-primary col-2 mt--20'>Complete Profile</button>
       </form>
+            </div>
+          </div>
+      
 
+        </div>
+        </section>
+        
+   
       <FooterFrontend />
     </div>
   );
